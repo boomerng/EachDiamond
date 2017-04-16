e404ee954ee393c36a9e40ecf30cfc112be55380
boomerng <mrrochen@gmail.com>
2017-04-16 15:17:19 -0400

    Initial commit

---
+++ Scripts/Banner.js
@@ -0,0 +1,27 @@
+﻿var currentBanner = 0;
+var banners = [];
+banners[0] = 'Content/Images/banners/1.jpg';
+banners[1] = 'Content/Images/banners/2.jpg';
+banners[2] = 'Content/Images/banners/3.jpg';
+banners[3] = 'Content/Images/banners/4.jpg';
+banners[4] = 'Content/Images/banners/5.jpg';
+
+function changeBanner() {
+    currentBanner++;
+    if (currentBanner > 4)
+        currentBanner = 0;
+    $('#imgBanner').attr("src", banners[currentBanner]);
+    $('#imgBanner').show(5000);
+    setTimeout(changeBanner, 10000);
+}
+
+$(document).ready(function () {
+    setTimeout(changeBanner, 10000);
+
+    $('#imgBanner').click(function () {
+        currentBanner++;
+        if (currentBanner > 4)
+            currentBanner = 0;
+        $('#imgBanner').attr("src", banners[currentBanner]);
+    });
+});
---
+++ Scripts/Shared.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    $("div.menu > ul > li").hover(
+        function () {
+            var ul = $(this).find("ul");
+            if (ul != undefined);
+            {
+                ul.slideDown();
+            }
+        },
+        function () {
+            var ul = $(this).find("ul");
+            if (ul != undefined);
+            {
+                ul.slideUp();
+            }
+        }
+    );
+});
